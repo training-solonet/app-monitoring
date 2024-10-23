a1bff21fdeeefa9b45b087131f35472012e204fa
Xyuraa <ifkararfian11@gmail.com>
2024-10-23 15:18:33 +0700

    buat migration & database

---
+++ database/migrations/2024_10_23_080942_tb_siswa_rpl.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tb_siswa_rpl', function (Blueprint $table) {
+            $table->id();
+            $table->enum('kategori', ['learning', 'project']);
+            $table->string('report');
+            $table->date('waktu_mulai');
+            $table->date('waktu_selesai');        
+            $table->enum('status', ['to do', 'doing','done']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tb_siswa_rpl');
+    }
+};
