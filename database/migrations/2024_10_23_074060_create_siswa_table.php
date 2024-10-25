de4de161e9a921603a61582f26b7b7b38fb2fa90
Xyuraa <ifkararfian11@gmail.com>
2024-10-25 13:07:28 +0700

    migration

--- database/migrations/2024_10_23_074060_create_siswa_table.php
+++ database/migrations/2024_10_23_074060_create_siswa_table.php
@@ -15,10 +15,12 @@ return new class extends Migration
     {
         Schema::create('siswa', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('id_kategori')->constrained('kategori');
             $table->text('report');
             $table->date('waktu_mulai');
             $table->date('waktu_selesai');
             $table->enum('status', ['to do','doing','done']);
+            $table->foreignId('id_login')->constrained('login');
             $table->timestamps();
         });
     }
