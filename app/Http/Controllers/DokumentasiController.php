dc29b06103f7ca1c9384fb5021de0713705527c7
Diandaru <149795529+Diandaru@users.noreply.github.com>
2025-02-13 04:18:36 +0000

    Fixes coding style

--- app/Http/Controllers/DokumentasiController.php
+++ app/Http/Controllers/DokumentasiController.php
@@ -2,17 +2,13 @@
 
 namespace App\Http\Controllers;
 
-
-use Illuminate\Http\Request;
 use Barryvdh\DomPDF\Facade\Pdf;
-
 
 class DokumentasiController extends Controller
 {
     public function downloadPDF()
     {
         $html = '<h1>Dokumentasi Monitoring</h1><p>Isi dari dokumentasi...</p>';
-
 
         $pdf = app('dompdf.wrapper');
         $pdf->loadHTML($html);
