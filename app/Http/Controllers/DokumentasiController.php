11667e26bbd3f5225d937e309406dffaec10a48b
Diandaru <irfansyah.fathiin@gmail.com>
2025-02-13 20:02:18 +0700

    Fix merge conflict in DokumentasiController

--- app/Http/Controllers/DokumentasiController.php
+++ app/Http/Controllers/DokumentasiController.php
@@ -2,19 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
 class DokumentasiController extends Controller
 {
-    public function downloadPDF()
+    public function cekFile()
     {
         $filePath = 'dokumentasi/Dokumentasi Monitoring.pdf';
 
-        // Perbaikan: Gunakan Storage::disk('public')
-        if (! Storage::disk('public')->exists($filePath)) {
+        // Gunakan Storage::disk('public') untuk mengecek keberadaan file
+        if (Storage::disk('public')->exists($filePath)) {
+            return response()->json(['status' => 'File ditemukan']);
+        } else {
             return response()->json(['error' => 'File tidak ditemukan.'], 404);
         }
-
-        return response()->download(storage_path("app/public/$filePath"));
     }
 }
