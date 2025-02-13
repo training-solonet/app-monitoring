fde94d412c569a2e4fd7c85e7673dfd9b721da8d
Diandaru <irfansyah.fathiin@gmail.com>
2025-02-13 20:13:29 +0700

    gatau mas

--- app/Http/Controllers/DokumentasiController.php
+++ app/Http/Controllers/DokumentasiController.php
@@ -1,16 +1,28 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Storage;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 class DokumentasiController extends Controller
 {
+    public function downloadPDF()
+    {
+        $filePath = storage_path('app/public/dokumentasi/Dokumentasi Monitoring.pdf');
+
+        // Cek apakah file ada
+        if (!file_exists($filePath)) {
+            return response()->json(['error' => 'File tidak ditemukan.'], 404);
+        }
+
+        // Mengembalikan file untuk di-download
+        return response()->download($filePath, 'Dokumentasi Monitoring.pdf');
+    }
+
     public function cekFile()
     {
         $filePath = 'dokumentasi/Dokumentasi Monitoring.pdf';
 
-        // Gunakan Storage::disk('public') untuk mengecek keberadaan file
         if (Storage::disk('public')->exists($filePath)) {
             return response()->json(['status' => 'File ditemukan']);
         } else {
